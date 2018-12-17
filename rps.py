9ea27b7b779b817c114f78704e02c549c6c4ec33
Jermaine Smikle <jermaine.smikle@aol.com>
2018-12-17 01:58:39 -0500

    changes

--- rps.py
+++ rps.py
@@ -13,24 +13,25 @@ def final_winner(player, computer):
     else:
         return "Its a draw!!!"
 
+def display_score(p,c):
+    print ("You:" + str(p))
+    print ("Computer:" + str(c))
+
 def start_game():
     games = 5
     player_wins = 0
     computer_wins = 0
     print ("Lets start")
     while (games > 0):
-        def display_score(p,c):
-            print ("You:" + str(p))
-            print ("Computer:" + str(c))
+        if (check_winner(player_wins, computer_wins)):
+            break
         options = ["rock", "paper", "scissors"]
 
         computer_choice = options[random.randint(0, len(options) - 1)]
         player_choice = input("Rock, paper, or scissors says... GO!!!! ")
         print (computer_choice)
 
-        if (check_winner(player_wins, computer_wins)):
-            break
-        elif(computer_choice == player_choice):
+        if(computer_choice == player_choice):
             print ("Match! Try Again")
         elif(computer_choice == "rock" and player_choice == "paper"):
             player_wins += 1
